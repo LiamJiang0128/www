7b17e25a19a6ae11c8bbdd5e142c3a011c927fbe
LiamJiang <87947447+LiamJiang0128@users.noreply.github.com>
2021-09-11 03:25:52 +0800

    Update changeimg.js

--- changeimg.js
+++ changeimg.js
@@ -1,4 +1,4 @@
-let imgArr = ["image1.jpeg", "image2.jpeg", "image3.jpeg", "image4.jpeg", "image5.jpeg"];
+let imgArr = ["image1.jpeg", "image2.jpeg", "image3.jpeg"];
 let img_index = 0;
 function  ChangeImgs() {
     //建立一個數組儲存照片的路徑
